e28eb8a4a3220f64b5e41e6c11d43409f14ea332
Adam Lee <alee8648@gmail.com>
2020-04-17 15:58:21 +0100

    Add all private information to an ignored file, getting it from the process instead

--- src/base.js
+++ src/base.js
@@ -1,15 +1,20 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+import apiKeys from './apiKeys.js';
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: 'AIzaSyBpnSt8UHlEymaxD77im2abzgKCETn6IPE',
-    authDomain: 'collaborative-p.firebaseapp.com',
-    databaseURL: 'https://collaborative-p.firebaseio.com',
-    projectId: 'collaborative-p',
-    storageBucket: 'collaborative-p.appspot.com',
-    messagingSenderId: '126141402215',
-    appId: '1:126141402215:web:07cfdabf6e2044dbced8c2',
-});
+if (!apiKeys) {
+    const apiKeys = {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+    };
+}
+
+const firebaseApp = firebase.initializeApp({ ...apiKeys });
 
 const base = Rebase.createClass(firebaseApp.database());
 
